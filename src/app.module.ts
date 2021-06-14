edfe7f09fc286eb212db4dc06cde2dcf1bcf924a
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-06-14 14:30:27 -0300

    [feat]: database connection
    connection to postgres via typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 import { DoctorsModule } from './doctors/doctors.module';
 
 @Module({
-  imports: [DoctorsModule],
+  imports: [
+    DoctorsModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+          migrations: [],
+        }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
