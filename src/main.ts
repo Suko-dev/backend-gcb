323653a0b729d120dcd05f369258596fb6035859
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-06-14 11:58:33 -0300

    [feat]: swagger
    used swagger for api documentation at 'localhost:300/api-documentation'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Doctor register')
+    .setDescription('CRUD for doctors')
+    .setVersion('1.0')
+    .addTag('doctor')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
