b67b716eac482e63c561c52cc56e467842f47567
Suko-Ribeiro <mvribeirodev@gmail.com>
2021-06-14 21:34:09 -0300

    [feat]: create validator
    implemented validators on dtos

--- src/doctors/dto/create-doctor.dto.ts
+++ src/doctors/dto/create-doctor.dto.ts
@@ -1,7 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Length,
+  Max,
+} from 'class-validator';
+
 export class CreateDoctorDto {
+  @ApiProperty({ example: 'cleyton' })
+  @IsString()
+  @IsNotEmpty()
+  @Length(5, 120)
   name: string;
+
+  @ApiProperty({ example: 1234567 })
+  @IsNotEmpty()
+  @Max(9999999)
+  @IsNumber()
   crm: number;
+
+  @ApiProperty({ example: 36038030 })
+  @IsNotEmpty()
+  @IsNumber()
   cep: number;
+
+  @ApiProperty({ example: 3230003000 })
+  @IsNotEmpty()
+  @IsNumber()
   phone: number;
+
+  @ApiProperty({ example: 32988888888 })
+  @IsNotEmpty()
+  @IsNumber()
   cellphone: number;
+
+  @ApiProperty({ example: ['Buco maxilo', 'Cardiologia infantil'] })
+  @IsNotEmpty()
+  @IsArray()
+  @ArrayMinSize(2)
+  specialties: string[];
 }
